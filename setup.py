7c97aa5e699041b8c24ad30e1d6ef1d16c61cde9
hugovk <hugovk@users.noreply.github.com>
2014-11-01 15:51:05 +0200

    Simple setup.py, so now: pip install fino

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="fino",
+    version="0.1.0",
+    description="Given an integer, output the Finnish word for that number.",
+    url="https://github.com/hugovk/fino",
+    author="hugovk",
+    keywords=["Finnish", "suomi", "numbers", "integers"],
+    py_modules=("fino",),
+    zip_safe=False,
+    )
+
+# End of file
