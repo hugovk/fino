078e0965cff1293d00014c747b2e4bcc79f13c27
Hugo <hugovk@users.noreply.github.com>
2018-11-18 16:58:39 +0200

    Drop support for Python 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -16,12 +16,10 @@ setup(
     keywords=["Finnish", "suomi", "numbers", "integers"],
     py_modules=("fino",),
     zip_safe=False,
-    python_requires=">=3.4",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 6 - Mature",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3 :: Only",
